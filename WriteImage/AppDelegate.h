4c83c3bad20a1b1624d7175cdba3eb7490974aa3
tony <liuyang86247683@sina.com>
2015-06-12 10:06:54 +0800

    Initial Commit

---
+++ WriteImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WriteImage
+//
+//  Created by 刘杨 on 15/6/12.
+//  Copyright (c) 2015年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WriteImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WriteImage
+//
+//  Created by 刘杨 on 15/6/12.
+//  Copyright (c) 2015年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
